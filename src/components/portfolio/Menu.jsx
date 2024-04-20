c9427cfa1f3b4954a6b03e95ad6c0e17e66fbbed
AbdulazizRus <coderdim@gmail.com>
2024-04-20 22:24:37 +0500

    asdas

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -9,6 +9,7 @@ import Work8 from '../../assests/taskfrontend19.netlify.app.png'
 import Work9 from '../../assests/TASK.png'
 import Work10 from '../../assests/Uzum projects.png'
 import Work11 from '../../assests/prudplus.png'
+import Work12 from '../../assests/Theeth.png'
 
 const Menu = [
     {
@@ -20,6 +21,13 @@ const Menu = [
     },
     {
         id: 2,
+        image: Work12,
+        title: "Eco Dental",
+        category: "E-Commerce",
+        link: "https://medicene.vercel.app"
+    },
+    {
+        id: 3,
         image: Work1,
         title: "ReactJS shopping shoes",
         category: "E-Commerce",
@@ -27,63 +35,63 @@ const Menu = [
     },
     
     {
-        id: 3,
+        id: 4,
         image: Work8,
         title: "Online Shop",
         category: "E-Commerce",
         link: "https://taskfrontend19.netlify.app"
     },
     {
-        id: 4,
+        id: 5,
         image: Work9,
         title: "Vex Products",
         category: "E-Commerce",
         link: ""
     },
     {
-        id: 5,
+        id: 6,
         image: Work4,
         title: "Portfolio WebSite",
         category: "Branding",
         link: "https://abdulazizrustamov.netlify.app" 
     },
     {
-        id: 6,
+        id: 7,
         image: Work10,
         title: "Uzum market Clone",
         category: "Clone",
-        link: "uzum.com"
+        link: "https://uzum-shop-demo.vercel.app/"
     },
     {
-        id: 7,
+        id: 8,
         image: Work6,
         title: "Calculator",
         category: "Small Projects",
         link: "https://calculator111.netlify.app"  
     },
     {
-        id: 8,
+        id: 9,
         image: Work7,
         title: "Resume",
         category: "Resume",
         link: "https://t.me/+xiaKJ_hOsp44ZGJi"
     },
     {
-        id: 9,
+        id: 10,
         image: Work2,
         title: "Chess Game",
         category: "Small Projects" ,
         link: "https://chessgame1.netlify.app"
     },
     {
-        id: 10,
+        id: 11,
         image: Work3,
         title: "Age Calculator",
         category: "Small Projects",
         link: "https://agecalculatorjs12.netlify.app" 
     },
     {
-        id: 11,
+        id: 12,
         image: Work5,
         title: "Pomidoro Timer",
         category: "Small Projects",
