32f83a8340be58dbebbec333d8c29af81fe7cabc
AbdulazizRus <coderdim@gmail.com>
2024-05-18 17:01:38 +0500

    dsasa

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -12,6 +12,7 @@ import Work11 from '../../assests/prudplus.png'
 import Work12 from '../../assests/Theeth.png'
 import Work13 from '../../assests/Metsenat.png'
 import Work14 from '../../assests/BookShelf.png'
+import Work15 from '../../assests/FilterPro.png'
 
 const Menu = [
     {
@@ -23,13 +24,20 @@ const Menu = [
     },
     {
         id: 2,
+        image: Work15,
+        title: "Filter Product",
+        category: "E-Commerce",
+        link: "https://filterproduct.vercel.app/"
+    },
+    {
+        id: 3,
         image: Work12,
         title: "Eco Dental",
         category: "E-Commerce",
         link: "https://medicene.vercel.app"
     },
     {
-        id: 3,
+        id: 4,
         image: Work1,
         title: "ReactJS shopping shoes",
         category: "E-Commerce",
@@ -37,77 +45,77 @@ const Menu = [
     },
     
     {
-        id: 4,
+        id: 5,
         image: Work8,
         title: "Online Shop",
         category: "E-Commerce",
         link: "https://taskfrontend19.netlify.app"
     },
     {
-        id: 5,
+        id: 6,
         image: Work14,
         title: "BookShelf",
         category: "Branding",
         link: "https://book-shelf19.vercel.app/"
     },
     {
-        id: 6,
+        id: 7,
         image: Work9,
         title: "Vex Products",
         category: "E-Commerce",
         link: ""
     },
     {
-        id: 7,
+        id: 8,
         image: Work13,
         title: "Metsenat",
         category: "Branding",
         link: "https://metsenat19.vercel.app/"
     },
     {
-        id: 8,
+        id: 9,
         image: Work4,
         title: "Portfolio WebSite",
         category: "Branding",
         link: "https://abdulazizrustamov.netlify.app" 
     },
     {
-        id: 9,
+        id: 10,
         image: Work10,
         title: "Uzum market Clone",
         category: "Clone",
         link: "https://uzum-shop-demo.vercel.app/"
     },
     {
-        id: 10,
+        id: 11,
         image: Work6,
         title: "Calculator",
         category: "Small Projects",
         link: "https://calculator111.netlify.app"  
     },
     {
-        id: 11,
+        id: 12,
         image: Work7,
         title: "Resume",
         category: "Resume",
         link: "https://t.me/+xiaKJ_hOsp44ZGJi"
     },
     {
-        id: 12,
+        id: 13,
         image: Work2,
         title: "Chess Game",
         category: "Small Projects" ,
         link: "https://chessgame1.netlify.app"
     },
     {
-        id: 13,
+        id: 14,
         image: Work3,
         title: "Age Calculator",
         category: "Small Projects",
         link: "https://agecalculatorjs12.netlify.app" 
     },
     {
-        id: 14,
+        id: 15,
         image: Work5,
         title: "Pomidoro Timer",
         category: "Small Projects",
