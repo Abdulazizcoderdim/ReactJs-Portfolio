63637b0b1ca043e832a0d6a8c19328d9171e6e8e
AbdulazizRus <coderdim@gmail.com>
2024-06-06 21:23:20 +0500

    asdas

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -20,177 +20,177 @@ import Work19 from '../../assests/domiktut.png'
 import Work20 from '../../assests/epizodk6.png'
 import Work21 from '../../assests/istom.png'
 
-
 const Menu = [
-    {
-        id: 1,
-        image: Work11,
-        title: "Prid Plus",
-        category: "E-Commerce",
-        link: "https://prudplus.ru",
-        link2:"https://github.com/Great-Dev-Uz/Prud-Front/"
-    },
-    {
-        id: 1,
-        image: Work18,
-        title: "Summarize ",
-        category: "Branding",
-        link: "https://sumz19.netlify.app/",
-        link2:"https://github.com/Abdulazizcoderdim/Sumz-AI"
-    },
-    {
-        id: 2,
-        image: Work17,
-        title: "FOREST INVERONMENTAL",
-        category: "Branding",
-        link: "https://tasknew19.vercel.app/",
-        link2:"https://github.com/Abdulazizcoderdim/TaskNew"
-    },
-    {
-        id: 2,
-        image: Work16,
-        title: "Case Cobra",
-        category: "E-Commerce",
-        link: "https://casecobra19.vercel.app/",
-        link2:"https://github.com/Abdulazizcoderdim/E-Commerce-casecobra"
-    },
-    {
-        id: 2,
-        image: Work19,
-        title: "Domiktut",
-        category: "E-Commerce",
-        link: "https://domiktut.ru/",
-        link2:""
-    },
-    {
-        id: 2,
-        image: Work20,
-        title: "Epizodk",
-        category: "E-Commerce",
-        link: "https://www.epizodk6.beget.tech/",
-        link2:""
-    },
-    {
-        id: 2,
-        image: Work21,
-        title: "Istom Shop",
-        category: "E-Commerce",
-        link: "https://istom-shop.ru/",
-        link2:""
-    },
-    {
-        id: 3,
-        image: Work15,
-        title: "Filter Product",
-        category: "E-Commerce",
-        link: "https://filterproduct.vercel.app/",
-        link2:"https://github.com/Abdulazizcoderdim/Filter-Product"
-    },
-    {
-        id: 4,
-        image: Work12,
-        title: "Eco Dental",
-        category: "E-Commerce",
-        link: "https://medicene.vercel.app",
-        link2:"https://github.com/Abdulazizcoderdim/Medicene"
-    },
-    {
-        id: 5,
-        image: Work1,
-        title: "ReactJS shopping shoes",
-        category: "E-Commerce",
-        link: "https://taskfrontendcoder.netlify.app",
-        link2:""
-    },
-    
-    {
-        id: 6,
-        image: Work8,
-        title: "Online Shop",
-        category: "E-Commerce",
-        link: "https://taskfrontend19.netlify.app",
-        link2:""
-    },
-    {
-        id: 7,
-        image: Work14,
-        title: "BookShelf",
-        category: "Branding",
-        link: "https://book-shelf19.vercel.app/",
-        link2:"https://github.com/Abdulazizcoderdim/BookShelf"
-    },
-    {
-        id: 8,
-        image: Work9,
-        title: "Vex Products",
-        category: "E-Commerce",
-        link: "",
-        link2:"https://github.com/Abdulazizcoderdim/task-project-vex"
-    },
-    {
-        id: 9,
-        image: Work13,
-        title: "Metsenat",
-        category: "Branding",
-        link: "https://metsenat19.vercel.app/",
-        link2:"ithub.com/Abdulazizcoderdim/Metsenat"
-    },
-    {
-        id: 10,
-        image: Work4,
-        title: "Portfolio WebSite",
-        category: "Branding",
-        link: "https://abdulazizrustamov.netlify.app",
-        link2:""
-    },
-    {
-        id: 11,
-        image: Work10,
-        title: "Uzum market Clone",
-        category: "Clone",
-        link: "https://uzum-shop-demo.vercel.app/",
-        link2:"https://github.com/Abdulazizcoderdim/Uzum-shop-demo"
-    },
-    {
-        id: 12,
-        image: Work6,
-        title: "Calculator",
-        category: "Small Projects",
-        link: "https://calculator111.netlify.app",
-        link2:""  
-    },
-    {
-        id: 13,
-        image: Work7,
-        title: "Resume",
-        category: "Resume",
-        link: "https://drive.google.com/file/d/1E-zG6G854tFlr8wmvKuGD-fvcNtAqjdM/view",
-        link2:""
-    },
-    {
-        id: 14,
-        image: Work2,
-        title: "Chess Game",
-        category: "Small Projects" ,
-        link: "https://chessgame1.netlify.app",
-        link2:""
-    },
-    {
-        id: 15,
-        image: Work3,
-        title: "Age Calculator",
-        category: "Small Projects",
-        link: "https://agecalculatorjs12.netlify.app",
-        link2:"" 
-    },
-    {
-        id: 16,
-        image: Work5,
-        title: "Pomidoro Timer",
-        category: "Small Projects",
-        link: "https://pomidorotimer11.netlify.app",
-        link2:""  
-    }
-];
+  {
+    id: 1,
+    image: Work11,
+    title: 'Prid Plus',
+    category: 'E-Commerce',
+    link: 'https://prudplus.ru',
+    link2: 'https://github.com/Great-Dev-Uz/Prud-Front/',
+  },
+  {
+    id: 1,
+    image: Work18,
+    title: 'Summarize ',
+    category: 'Branding',
+    link: 'https://sumz19.netlify.app/',
+    link2: 'https://github.com/Abdulazizcoderdim/Sumz-AI',
+  },
+
+  {
+    id: 2,
+    image: Work16,
+    title: 'Case Cobra',
+    category: 'E-Commerce',
+    link: 'https://casecobra19.vercel.app/',
+    link2: 'https://github.com/Abdulazizcoderdim/E-Commerce-casecobra',
+  },
+  {
+    id: 2,
+    image: Work19,
+    title: 'Domiktut',
+    category: 'E-Commerce',
+    link: 'https://domiktut.ru/',
+    link2: '',
+  },
+  {
+    id: 2,
+    image: Work20,
+    title: 'Epizodk',
+    category: 'E-Commerce',
+    link: 'https://www.epizodk6.beget.tech/',
+    link2: '',
+  },
+  {
+    id: 2,
+    image: Work21,
+    title: 'Istom Shop',
+    category: 'E-Commerce',
+    link: 'https://istom-shop.ru/',
+    link2: '',
+  },
+  {
+    id: 2,
+    image: Work17,
+    title: 'FOREST INVERONMENTAL',
+    category: 'Branding',
+    link: 'https://tasknew19.vercel.app/',
+    link2: 'https://github.com/Abdulazizcoderdim/TaskNew',
+  },
+  {
+    id: 3,
+    image: Work15,
+    title: 'Filter Product',
+    category: 'E-Commerce',
+    link: 'https://filterproduct.vercel.app/',
+    link2: 'https://github.com/Abdulazizcoderdim/Filter-Product',
+  },
+  {
+    id: 4,
+    image: Work12,
+    title: 'Eco Dental',
+    category: 'E-Commerce',
+    link: 'https://medicene.vercel.app',
+    link2: 'https://github.com/Abdulazizcoderdim/Medicene',
+  },
+  {
+    id: 5,
+    image: Work1,
+    title: 'ReactJS shopping shoes',
+    category: 'E-Commerce',
+    link: 'https://taskfrontendcoder.netlify.app',
+    link2: '',
+  },
+
+  {
+    id: 6,
+    image: Work8,
+    title: 'Online Shop',
+    category: 'E-Commerce',
+    link: 'https://taskfrontend19.netlify.app',
+    link2: '',
+  },
+  {
+    id: 7,
+    image: Work14,
+    title: 'BookShelf',
+    category: 'Branding',
+    link: 'https://book-shelf19.vercel.app/',
+    link2: 'https://github.com/Abdulazizcoderdim/BookShelf',
+  },
+  {
+    id: 8,
+    image: Work9,
+    title: 'Vex Products',
+    category: 'E-Commerce',
+    link: '',
+    link2: 'https://github.com/Abdulazizcoderdim/task-project-vex',
+  },
+  {
+    id: 9,
+    image: Work13,
+    title: 'Metsenat',
+    category: 'Branding',
+    link: 'https://metsenat19.vercel.app/',
+    link2: 'ithub.com/Abdulazizcoderdim/Metsenat',
+  },
+  {
+    id: 10,
+    image: Work4,
+    title: 'Portfolio WebSite',
+    category: 'Branding',
+    link: 'https://abdulazizrustamov.netlify.app',
+    link2: '',
+  },
+  {
+    id: 11,
+    image: Work10,
+    title: 'Uzum market Clone',
+    category: 'Clone',
+    link: 'https://uzum-shop-demo.vercel.app/',
+    link2: 'https://github.com/Abdulazizcoderdim/Uzum-shop-demo',
+  },
+  {
+    id: 12,
+    image: Work6,
+    title: 'Calculator',
+    category: 'Small Projects',
+    link: 'https://calculator111.netlify.app',
+    link2: '',
+  },
+  {
+    id: 13,
+    image: Work7,
+    title: 'Resume',
+    category: 'Resume',
+    link: 'https://drive.google.com/file/d/1E-zG6G854tFlr8wmvKuGD-fvcNtAqjdM/view',
+    link2: '',
+  },
+  {
+    id: 14,
+    image: Work2,
+    title: 'Chess Game',
+    category: 'Small Projects',
+    link: 'https://chessgame1.netlify.app',
+    link2: '',
+  },
+  {
+    id: 15,
+    image: Work3,
+    title: 'Age Calculator',
+    category: 'Small Projects',
+    link: 'https://agecalculatorjs12.netlify.app',
+    link2: '',
+  },
+  {
+    id: 16,
+    image: Work5,
+    title: 'Pomidoro Timer',
+    category: 'Small Projects',
+    link: 'https://pomidorotimer11.netlify.app',
+    link2: '',
+  },
+]
 
 export default Menu
