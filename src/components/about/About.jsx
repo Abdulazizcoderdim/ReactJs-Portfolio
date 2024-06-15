815fb6637be3e1eea72fbfd837bbb13cf3e2913d
AbdulazizRus <coderdim@gmail.com>
2024-06-15 22:14:20 +0500

    asda

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,63 +1,66 @@
 import './About.css'
 import Image from '../../assests/man.png'
-import AboutBox from './AboutBox'
 
 const About = () => {
-    return (
-        <section className="about container section" id='about'>
-            <h2 className="section__title">About Me</h2>
+  return (
+    <section className="about container section" id="about">
+      <h2 className="section__title">About Me</h2>
 
-            <div className="section__container grid">
-                <img src={Image} alt="" className='about__img' />
+      <div className="section__container grid">
+        <img src={Image} alt="" className="about__img" />
 
-                <div className="about__data grid">
-                    <div className="about__info ">
-                        <p className="about__description">I am Abdulaziz Rustamov, Web Developer and Grafic Designer from Uzbekistan. I have rich experience in web site design and building and customization, also I am good at ReactJS.</p>
-                        <a href="./Resume1.pdf" download className='btn'>Download CV</a>
-                    </div>
+        <div className="about__data grid">
+          <div className="about__info ">
+            <p className="about__description">
+              I am Abdulaziz Rustamov, Web Developer and Grafic Designer from
+              Uzbekistan. I have rich experience in web site design and building
+              and customization, also I am good at ReactJS.
+            </p>
+            <a href="/ResumeAb.pdf" download className="btn">
+              Download CV
+            </a>
+          </div>
 
-                    <div className="about__skills grid">
-                        <div className="skills__data">
-                            <div className="skills__titles">
-                                <h3 className="skills__name">JavaScript</h3>
-                                <span className="skills__number ">90%</span>
-                            </div>
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">JavaScript</h3>
+                <span className="skills__number ">90%</span>
+              </div>
 
-                            <div className="skills__bar">
-                                <span className="skills__percentage development"></span>
-                            </div>
-                        </div>
-
-                        <div className="skills__data">
-                            <div className="skills__titles">
-                                <h3 className="skills__name">ReactJs</h3>
-                                <span className="skills__number">85%</span>
-                            </div>
-
-                            <div className="skills__bar">
-                                <span className="skills__percentage ui__design"></span>
-                            </div>
-                        </div>
-
-                        <div className="skills__data">
-                            <div className="skills__titles">
-                                <h3 className="skills__name">NodeJs</h3>
-                                <span className="skills__number ">80%</span>
-                            </div>
-
-                            <div className="skills__bar">
-                                <span className="skills__percentage design"></span>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
+              <div className="skills__bar">
+                <span className="skills__percentage development"></span>
+              </div>
             </div>
 
-            {/* <AboutBox/> */}
-        </section>
-    )
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">ReactJs</h3>
+                <span className="skills__number">85%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage ui__design"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">NodeJs</h3>
+                <span className="skills__number ">80%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage design"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <AboutBox/> */}
+    </section>
+  )
 }
 
 export default About
