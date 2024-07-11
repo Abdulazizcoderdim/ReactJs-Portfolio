9f52bc0846bad20af871961016631769bd5c4965
AbdulazizRus <coderdim@gmail.com>
2024-07-11 20:33:36 +0500

    assa

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -69,7 +69,7 @@ const Sidebar = () => {
              </nav>
 
              <div className="nav__footer">
-                <span className="copyright">&copy; 2023 - 2024.</span>
+                <span className="copyright">&copy; 2020 - {new Date().getFullYear()}.</span>
              </div>
         </aside>
 
